c3b92ceacb15e1fcac9bbcbc3e0287089df5eebc
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-08 00:46:23 -0700

    Add project files.

---
+++ ItrerativeInorderTraversalBinaryTree/IterativeInOrderTeravelsal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItrerativeInorderTraversalBinaryTree
+{
+    class IterativeInOrderTeravelsal
+    {
+        public List<int> IterativeInorder(Node root)
+        {
+            List<int> li = new List<int>();
+            if (root == null)
+                return li;
+
+            Stack<Node> S = new Stack<Node>();
+
+            while (true)
+            {
+                if(root != null)
+                {
+                    S.Push(root);
+                    root = root.left;
+                }
+                else
+                {
+                    if (S.Count == 0)
+                        break;
+
+                    root = S.Pop();
+                    li.Add(root.data);
+                    root = root.right;
+                }
+            }
+            return li;
+        }
+    }
+}
---
+++ ItrerativeInorderTraversalBinaryTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ItrerativeInorderTraversalBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(10);
+            tree.left = new Node(0);
+            tree.left.left = new Node(5);
+            tree.left.right = new Node(6);
+            tree.right = new Node(-10);
+            tree.right.left = new Node(11);
+
+            IterativeInOrderTeravelsal II = new IterativeInOrderTeravelsal();
+            
+
+            Console.WriteLine(string.Join(" , " , II.IterativeInorder(tree)));
+            Console.ReadLine();
+        }
+    }
+}
